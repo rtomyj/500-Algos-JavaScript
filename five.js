b7d6ba1d35577a95af652a637f22f74e6581485c
Javi Gomez <rtomyj@gmail.com>
2019-03-17 14:49:13 -0500

    new algo done

---
+++ five.js
@@ -0,0 +1,44 @@
+"using strict"
+let items = [1, 2, 3, 4, 4]
+//let items = [1, 2, 3, 4, 2]
+//sortAndFind(items)
+hashAndFind(items)
+
+
+function sortAndFind(items)
+{
+	items.sort()
+	for (let i = 1; i < items.length; i++)
+	{
+		if (items[i] === items[i -1])
+		{
+			alert(`Dup element is ${items[i]}`)
+			return
+		}
+	}
+}
+
+function hashAndFind(items)
+{
+	let min = items[0]
+	let max = items[0]
+	items.slice(1).forEach(item =>
+		{
+			if (item < min)	min = item
+			if (item > max)	max = item
+		}
+	)
+	let range = max - min
+	let hash = Array(range).fill(false)
+	for (let i = 0; i < items.length; i++)
+	{
+		let value = items[i]
+		if (hash[value - min] === true)
+		{
+			alert(`Dup element is ${value}`)
+			return
+		}
+		else 	hash[value - min] = true
+	}
+
+}
