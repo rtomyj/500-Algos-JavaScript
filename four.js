b48e49059644848d641300103cd6ea1498b81565
Javi Gomez <rtomyj@gmail.com>
2019-03-17 12:39:28 -0500

    new algo - done

---
+++ four.js
@@ -0,0 +1,37 @@
+"using strict"
+items = [1, 0, 1, 0, 1, 0, 0, 1]
+//fillAndAdd(items, leftMostValue=1)
+mergeTwoBinaryArrays(items, leftMostValue = 1)
+
+function fillAndAdd(items, leftMostValue = 0)
+{
+	let rightMostValue = Number(!Boolean(leftMostValue))
+	let leftMostIndex = 0
+
+	for (let i = 0; i < items.length; i++)
+	{
+		if (items[i] === leftMostValue)	items[leftMostIndex++] = leftMostValue
+	}
+	for (let i = leftMostIndex; i < items.length; i++)	items[i] = rightMostValue
+
+	alert(items)
+}
+
+function mergeTwoBinaryArrays(items, leftMostValue = 0)
+{
+	let leftValueOccurence = 0;
+	let rightValueOccurence = 0;
+
+	for (let i = 0; i < items.length; i++)
+	{
+		if (items[i] === leftMostValue)	++leftValueOccurence
+		else	++rightValueOccurence
+	}
+	
+	let sorted = []
+	let rightMostValue = Number(!Boolean(leftMostValue))
+	sorted.push(...Array(leftValueOccurence).fill(leftMostValue))
+	sorted.push(...Array(rightValueOccurence).fill(rightMostValue))
+
+	alert(sorted)
+}
